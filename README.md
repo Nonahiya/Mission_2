9763f734a296c4b93ab6e3cbfa62ac051e22459b
Enrico <enricowakiman9@gmail.com>
2022-10-11 12:54:07 +0700

    test

---
+++ README.md
@@ -0,0 +1,6 @@
+# mission_1
+
+Digital Entrepreneurship Product Development
+Flutter Widgets Assignment 1
+
+0706012010036 - Enrico Almer Tahara
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:mission_1/views/pages.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const HomePage(),        
+    });  }
+}
