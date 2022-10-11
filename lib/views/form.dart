d1dc557d728d024bde124a76996538d13d572a7e
Enrico <enricowakiman9@gmail.com>
2022-10-11 20:04:14 +0700

    complete 100%

--- lib/views/form.dart
+++ lib/views/form.dart
@@ -1,1 +1,324 @@
 part of 'pages.dart';
+
+class FormPage extends StatefulWidget {
+  const FormPage({Key? key}) : super(key: key);
+
+  @override
+  _FormPageState createState() => _FormPageState();
+}
+
+class _FormPageState extends State<FormPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  final _loginKey = GlobalKey<FormState>();
+  final ctrlName = TextEditingController();
+  final ctrlEmail = TextEditingController();
+  // final ctrlPass = TextEditingController();
+  final ctrlNumber = TextEditingController();
+  final ctrlCity = TextEditingController();
+  bool isHidden = true;
+
+  @override
+  void dispose() {
+    ctrlName.dispose();
+    ctrlEmail.dispose();
+    // ctrlPass.dispose();
+    ctrlNumber.dispose();
+    ctrlCity.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Booking Form'),
+          centerTitle: true,
+          backgroundColor: const Color(0XFF79018C),
+        ),
+        body: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: <Color>[
+                  Color.fromARGB(255, 185, 108, 196),
+                  Color.fromARGB(255, 255, 255, 255),
+                ],
+              ),
+            ),
+            padding: const EdgeInsets.fromLTRB(24, 16, 24, 16),
+            child: Form(
+              key: _loginKey,
+              child: Column(children: [
+                const SizedBox(height: 16),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: "Full Name",
+                    labelStyle: TextStyle(color: Colors.black),
+                    prefixIcon: Icon(Icons.person, color: Colors.black),
+                    filled: true,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0XFF79018C),
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.black,
+                      ),
+                    ),
+                    errorBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.red,
+                      ),
+                    ),
+                    focusedErrorBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.red,
+                      ),
+                    ),
+                  ),
+                  controller: ctrlName,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: (value) {
+                    return value.toString().isEmpty
+                        ? 'Full name must not be empty!'
+                        : null;
+                  },
+                ),
+
+                const SizedBox(height: 16),
+                TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: const InputDecoration(
+                    labelText: "Email",
+                    labelStyle: TextStyle(color: Colors.black),
+                    prefixIcon: Icon(Icons.email, color: Colors.black),
+                    filled: true,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0XFF79018C),
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.black,
+                      ),
+                    ),
+                    errorBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.red,
+                      ),
+                    ),
+                    focusedErrorBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.red,
+                      ),
+                    ),
+                  ),
+                  controller: ctrlEmail,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: (value) {
+                    return !EmailValidator.validate(value.toString())
+                        ? 'Invalid email!'
+                        : null;
+                  },
+                ),
+
+                // const SizedBox(height: 16),
+                // TextFormField(
+                //   obscureText: isHidden,
+                //   decoration: InputDecoration(
+                //     labelText: "Password",
+                //     suffixIcon: GestureDetector(
+                //       onTap: () {
+                //         setState(() {
+                //           isHidden = !isHidden;
+                //         });
+                //       },
+                //       child: Icon(
+                //           isHidden ? Icons.visibility : Icons.visibility_off,
+                //           color: Colors.black),
+                //     ),
+                //     labelStyle: const TextStyle(color: Colors.black),
+                //     prefixIcon: const Icon(Icons.lock, color: Colors.black),
+                //     filled: true,
+                //     focusedBorder: const OutlineInputBorder(
+                //       borderSide: BorderSide(
+                //         color: Color(0XFF79018C),
+                //       ),
+                //     ),
+                //     enabledBorder: const OutlineInputBorder(
+                //       borderSide: BorderSide(
+                //         color: Colors.black,
+                //       ),
+                //     ),
+                //     errorBorder: const OutlineInputBorder(
+                //       borderSide: BorderSide(
+                //         color: Colors.red,
+                //       ),
+                //     ),
+                //     focusedErrorBorder: const OutlineInputBorder(
+                //       borderSide: BorderSide(
+                //         color: Colors.red,
+                //       ),
+                //     ),
+                //   ),
+                //   controller: ctrlPass,
+                //   autovalidateMode: AutovalidateMode.onUserInteraction,
+                //   validator: (value) {
+                //     return value.toString().length < 8
+                //         ? 'Password must be at least 8 characters!'
+                //         : null;
+                //   },
+                // ),
+
+                const SizedBox(height: 16),
+                TextFormField(
+                  keyboardType: TextInputType.phone,
+                  decoration: const InputDecoration(
+                    labelText: "Phone Number",
+                    labelStyle: TextStyle(color: Colors.black),
+                    prefixIcon: Icon(Icons.phone_android, color: Colors.black),
+                    filled: true,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0XFF79018C),
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.black,
+                      ),
+                    ),
+                    errorBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.red,
+                      ),
+                    ),
+                    focusedErrorBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.red,
+                      ),
+                    ),
+                  ),
+                  controller: ctrlNumber,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: (value) {
+                    return value.toString().length != 10
+                        ? 'Phone number must be 10 characters!'
+                        : null;
+                  },
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: "City",
+                    labelStyle: TextStyle(color: Colors.black),
+                    prefixIcon: Icon(Icons.location_city, color: Colors.black),
+                    filled: true,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0XFF79018C),
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.black,
+                      ),
+                    ),
+                    errorBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.red,
+                      ),
+                    ),
+                    focusedErrorBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.red,
+                      ),
+                    ),
+                  ),
+                  controller: ctrlCity,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: (value) {
+                    return value.toString().length < 2
+                        ? 'City must be at least 2 characters!'
+                        : null;
+                  },
+                ),
+                const SizedBox(height: 16),
+                ElevatedButton.icon(
+                    style: ElevatedButton.styleFrom(
+                        shape: const StadiumBorder(),
+                        backgroundColor: const Color(0XFF79018C)),
+                    onPressed: () {
+                      if (_loginKey.currentState!.validate()) {
+                        showDialog(
+                          context: context,
+                          builder: (context) => AlertDialog(
+                            title: const Text(
+                              'Booking Confirmation',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            content: Text(
+                              // 'Email: ${ctrlEmail.text}\nPass: ${ctrlPass.text}\nPhone Number: ${ctrlNumber.text}\nCity: ${ctrlCity.text}',
+                              'Name: ${ctrlName.text}\nEmail: ${ctrlEmail.text}\nPhone: ${ctrlNumber.text}\nCity: ${ctrlCity.text}',
+                            ),
+                            actions: [
+                              TextButton(
+                                  style: TextButton.styleFrom(
+                                      backgroundColor: Colors.white,
+                                      foregroundColor: const Color(0XFF79018C)),
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: const Text('Back')),
+                              TextButton(
+                                  style: TextButton.styleFrom(
+                                      backgroundColor: Colors.white,
+                                      foregroundColor: const Color(0XFF79018C)),
+                                  onPressed: () {
+                                    Navigator.pushAndRemoveUntil<dynamic>(
+                                        context,
+                                        MaterialPageRoute<dynamic>(
+                                            builder: (conrext) =>
+                                                const HomePage()),
+                                        (route) => false);
+                                  },
+                                  child: const Text('Continue'))
+                            ],
+                          ),
+                        );
+                      } else {
+                        showDialog(
+                          context: context,
+                          builder: (context) => AlertDialog(
+                            title: const Text(
+                              'Booking Failed',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            content: const Text('Please fill all the fields!'),
+                            actions: [
+                              TextButton(
+                                  style: TextButton.styleFrom(
+                                      backgroundColor: Colors.white,
+                                      foregroundColor: const Color(0XFF79018C)),
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: const Text('Try Again'))
+                            ],
+                          ),
+                        );
+                      }
+                    },
+                    icon: const Icon(Icons.save),
+                    label: const Text("Book Now")),
+              ]),
+            )));
+  }
+}
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -31,106 +31,125 @@ class _HomePageState extends State<HomePage> {
         ),
         width: double.infinity,
         height: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            const Flexible(
-              flex: 9,
-              child: Image(
-                  image: AssetImage('assets/images/shirai1.png'),
-                  fit: BoxFit.cover),
-            ),
-            const SizedBox(height: 10),
-            Flexible(
-              flex: 3,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                      width: 115,
-                      clipBehavior: Clip.hardEdge,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                      child: const AspectRatio(
-                        aspectRatio: 1 / 1,
-                        child: Image(
-                            image: AssetImage('assets/images/shirai2.png'),
-                            fit: BoxFit.cover),
-                      )),
-                  Container(
-                      width: 115,
-                      clipBehavior: Clip.hardEdge,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                      child: const AspectRatio(
-                        aspectRatio: 1 / 1,
-                        child: Image(
-                            image: AssetImage('assets/images/shirai3.jpg'),
-                            fit: BoxFit.cover),
-                      )),
-                  Container(
-                      width: 115,
-                      clipBehavior: Clip.hardEdge,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                      child: const AspectRatio(
-                        aspectRatio: 1 / 1,
-                        child: Image(
-                            image: AssetImage('assets/images/shirai4.png'),
-                            fit: BoxFit.cover),
-                      )),
-                  Container(
-                      width: 115,
-                      clipBehavior: Clip.hardEdge,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                      child: const AspectRatio(
-                        aspectRatio: 1 / 1,
-                        child: Image(
-                            image: AssetImage('assets/images/shirai5.jpg'),
-                            fit: BoxFit.cover),
-                      )),
-                ],
+        child: Stack(children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const Flexible(
+                flex: 9,
+                child: Image(
+                    image: AssetImage('assets/images/shirai1.png'),
+                    fit: BoxFit.cover),
               ),
-            ),
-            const SizedBox(height: 15),
-            const Flexible(
-              flex: 1,
-              child: Text(
-                "Shirai Kuroko",
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              const SizedBox(height: 10),
+              Flexible(
+                flex: 3,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                        width: 115,
+                        clipBehavior: Clip.hardEdge,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        child: const AspectRatio(
+                          aspectRatio: 1 / 1,
+                          child: Image(
+                              image: AssetImage('assets/images/shirai2.png'),
+                              fit: BoxFit.cover),
+                        )),
+                    Container(
+                        width: 115,
+                        clipBehavior: Clip.hardEdge,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        child: const AspectRatio(
+                          aspectRatio: 1 / 1,
+                          child: Image(
+                              image: AssetImage('assets/images/shirai3.jpg'),
+                              fit: BoxFit.cover),
+                        )),
+                    Container(
+                        width: 115,
+                        clipBehavior: Clip.hardEdge,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        child: const AspectRatio(
+                          aspectRatio: 1 / 1,
+                          child: Image(
+                              image: AssetImage('assets/images/shirai4.png'),
+                              fit: BoxFit.cover),
+                        )),
+                    Container(
+                        width: 115,
+                        clipBehavior: Clip.hardEdge,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        child: const AspectRatio(
+                          aspectRatio: 1 / 1,
+                          child: Image(
+                              image: AssetImage('assets/images/shirai5.jpg'),
+                              fit: BoxFit.cover),
+                        )),
+                  ],
+                ),
               ),
-            ),
-            Flexible(
-              flex: 5,
-              child: Container(
-                margin: const EdgeInsets.only(left: 7.5),
-                child: ListView(
-                    scrollDirection: Axis.vertical,
-                    children: const <Widget>[
-                      Flexible(
-                        child: Text(
-                            "Shirai Kuroko (白井 黒子 Shirai Kuroko) is a Level 4 esper and a student of Tokiwadai Middle School, as well as a member of Judgment. She can usually be found accompanying the third-ranked Level 5 esper Misaka Mikoto — her best friend, partner, and roommate. Though a minor character in Toaru Majutsu no Index, she gets a starring role alongside Mikoto in the Toaru Kagaku no Railgun sidestory."),
-                      ),
-                      SizedBox(height: 10),
-                      Flexible(
-                        child: Text(
-                            "The name Shirai Kuroko is quite interesting because it contains both the meanings of 'white' and 'black'. The family name 'Shirai' (kanji: 白井) can be literally interpreted as 'white' plus '(water) well'; while the given name 'Kuroko' (kanji: 黒子) is more interesting: as a name, it can be interpreted as the stem 'Kuro-' (meaning 'black') plus the suffix '-ko' - which is commonly seen in Japanese female names (other examples include Saten Ruiko and Kongou Mitsuko); while not used as a name, the term 'kuroko' means stagehands in traditional Japanese theater, who dress all in black."),
-                      ),
-                      SizedBox(height: 10),
-                      Flexible(
-                        child: Text(
-                            "Kuroko is a girl with tawny hair tied into two curly pigtails; the rest of her hair is left to hang as two rows of fringes divided in the middle. Her pigtails are tied in place by a red bow each. Her hair is naturally curly and as such needs to get a straight perm for it. With 152 cm she isn't very big and with a flat chest her appearance is the usual appearance of an average girl at her age. As per ruling of both Academy City and Judgment, she wears the uniform of Tokiwadai Middle School and the Judgment armband though the latter only during on duty. On her thighs, she secures a pair of black straps where she stores the metal spikes used to secure criminals."),
-                      ),
-                    ]),
+              const SizedBox(height: 15),
+              const Flexible(
+                flex: 1,
+                child: Text(
+                  "Shirai Kuroko",
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
               ),
+              Flexible(
+                flex: 5,
+                child: Container(
+                  margin: const EdgeInsets.only(left: 7.5),
+                  child: ListView(
+                      scrollDirection: Axis.vertical,
+                      children: const <Widget>[
+                        Flexible(
+                          child: Text(
+                              "Shirai Kuroko (白井 黒子 Shirai Kuroko) is a Level 4 esper and a student of Tokiwadai Middle School, as well as a member of Judgment. She can usually be found accompanying the third-ranked Level 5 esper Misaka Mikoto — her best friend, partner, and roommate. Though a minor character in Toaru Majutsu no Index, she gets a starring role alongside Mikoto in the Toaru Kagaku no Railgun sidestory."),
+                        ),
+                        SizedBox(height: 10),
+                        Flexible(
+                          child: Text(
+                              "The name Shirai Kuroko is quite interesting because it contains both the meanings of 'white' and 'black'. The family name 'Shirai' (kanji: 白井) can be literally interpreted as 'white' plus '(water) well'; while the given name 'Kuroko' (kanji: 黒子) is more interesting: as a name, it can be interpreted as the stem 'Kuro-' (meaning 'black') plus the suffix '-ko' - which is commonly seen in Japanese female names (other examples include Saten Ruiko and Kongou Mitsuko); while not used as a name, the term 'kuroko' means stagehands in traditional Japanese theater, who dress all in black."),
+                        ),
+                        SizedBox(height: 10),
+                        Flexible(
+                          child: Text(
+                              "Kuroko is a girl with tawny hair tied into two curly pigtails; the rest of her hair is left to hang as two rows of fringes divided in the middle. Her pigtails are tied in place by a red bow each. Her hair is naturally curly and as such needs to get a straight perm for it. With 152 cm she isn't very big and with a flat chest her appearance is the usual appearance of an average girl at her age. As per ruling of both Academy City and Judgment, she wears the uniform of Tokiwadai Middle School and the Judgment armband though the latter only during on duty. On her thighs, she secures a pair of black straps where she stores the metal spikes used to secure criminals."),
+                        ),
+                      ]),
+                ),
+              ),
+            ],
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Container(
+              padding: const EdgeInsets.only(bottom: 15.0, right: 15.0),
+              child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      shape: const StadiumBorder(),
+                      backgroundColor: const Color(0XFF79018C)),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const FormPage()));
+                  },
+                  child: const Text('Book Now')),
             ),
-          ],
-        ),
+          ),
+        ]),
       ),
       floatingActionButton: Padding(
         padding: const EdgeInsets.only(top: 100.0),
